9a686d905bffdae63371db65c556d5aa7ef3ea67
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-07-04 15:42:09 -0300

    ajuste scrollbar

--- src/pages/Layout/styles.ts
+++ src/pages/Layout/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-height: (100vh - 60px);
+  max-height: (100vh);
   height: 100%;
 `;
 
@@ -10,4 +10,28 @@ export const Wrapper = styled.div`
   max-height: calc(100vh - 302px);
   height: 100%;
   scroll-behavior: smooth;
+
+  --sb-track-color: #232e33;
+  --sb-thumb-color: #ff6b00;
+  --sb-size: 5px;
+
+  &::-webkit-scrollbar {
+    width: var(--sb-size);
+  }
+
+  &::-webkit-scrollbar-track {
+    background: var(--sb-track-color);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--sb-thumb-color);
+    border-radius: 3px;
+  }
+
+  @supports not selector(::-webkit-scrollbar) {
+    & {
+      scrollbar-color: var(--sb-thumb-color) var(--sb-track-color);
+    }
+  }
 `;
