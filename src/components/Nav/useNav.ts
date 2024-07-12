bfe4ea7d1842a7d58fa9415e17b8ae2fa2d71313
Danrley Ney <danrley.dev@gmail.com>
2024-07-12 18:03:30 -0300

    fix: ajustes

--- src/components/Nav/useNav.ts
+++ src/components/Nav/useNav.ts
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { useAppContext } from "../../context/AppContext";
 import { useEffect, useState } from "react";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { IDataDeliveryUser } from "../../model/Product";
@@ -17,17 +16,15 @@ const userDelivery: IDataDeliveryUser = {
 export const useNav = () => {
   const [dataDelivery] = useLocalStorage<IDataDeliveryUser>({ storageKey: "@delivery", initialValue: userDelivery })
   const navigate = useNavigate();
-  const { bagProducts } = useAppContext();
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-
-    const temp = bagProducts
+    const temp = dataDelivery.products
       .map((i) => i.price * i.quantity)
       .reduce((acc, price) => acc + price, 0);
 
     setTotal(temp);
-  }, [bagProducts]);
+  }, [dataDelivery]);
 
   return { navigate, total, dataDelivery };
 };
