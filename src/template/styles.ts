280639e390f43aae732c023ea8cad9669f5eb181
Danrley Ney <danrley.dev@gmail.com>
2024-07-20 20:12:33 -0300

    fix: ajuste css

--- src/template/styles.ts
+++ src/template/styles.ts
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
   overflow-y: scroll;
   height: 100%;
-  max-height: calc(100vh - 100px);
+  max-height: calc(100vh - 130px);
   scroll-behavior: smooth;
 `;
 
