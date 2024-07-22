a23cb4e5881a2355d26f8f7b0122f49b535e6ac8
Danrley Ney <danrley.dev@gmail.com>
2024-07-22 15:56:12 -0300

    styles: responsive

--- src/pages/Address/styles.ts
+++ src/pages/Address/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  height: 100vh; 
+  
   background-color: #FAFAFA;
   
 `;
--- src/pages/Layout/styles.ts
+++ src/pages/Layout/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
 
 export const Wrapper = styled.div`
   overflow-y: scroll;
-  max-height: calc(100vh - 260px);
+  max-height: calc(100vh - 230px);
   /* height: 100%; */
   scroll-behavior: smooth;
 `;
